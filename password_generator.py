a43b9a99dc9025638b0354b7af4814cc718c1502
Michał Świdziński <michalswidzinski01@gmail.com>
2022-03-13 17:12:08 +0100

    Add additional tests for password length

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,20 @@
 import random
 
 
+class PasswordTooShortException(Exception):
+    pass
+
+
+class PasswordTooLongException(Exception):
+    pass
+
+
 def password_generator(pass_length = 8):
+    if pass_length < 8:
+        raise PasswordTooShortException
+        
+    if pass_length > 32:
+        raise PasswordTooLongException
 
     characters_range = range(33, 127)
     password = ''
