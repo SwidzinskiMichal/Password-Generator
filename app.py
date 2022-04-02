4b343f5b0161d0642d8b8be855d20acf8e84e58c
Michał Świdziński <michalswidzinski01@gmail.com>
2022-04-02 17:50:31 +0200

    Updat README and app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-
+from password_generator import password_generator
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def generate_password():
+    password = password_generator()
+    return "Your password is: " + password
