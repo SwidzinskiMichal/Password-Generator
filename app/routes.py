033551fb0ee4bd74b93d33f28307508fbab60be8
Michał Świdziński <michalswidzinski01@gmail.com>
2022-04-02 19:11:28 +0200

    Removed unnecessary code

--- app/routes.py
+++ app/routes.py
@@ -6,5 +6,4 @@ from app.password_generator import password_generator
 @app.route('/')
 def generate_password():
     password = password_generator()
-    print(password)
     return escape("Your password is: " + password)
