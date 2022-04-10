d93eef21a50013ca667d71e3f8fd83a43b1fc34f
Michał Świdziński <michalswidzinski01@gmail.com>
2022-04-10 21:20:41 +0200

    Add option to chose charsets

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
-from flask import escape
-
+from flask import escape, render_template, request
 from app import app
 from app.password_generator import password_generator
 
-@app.route('/')
-def generate_password():
-    password = password_generator()
-    return escape("Your password is: " + password)
+
+@app.route('/', methods=['GET', 'POST'])
+def checkboxes():
+    if request.method == "POST":
+        charset_chosen = request.form.getlist("mycheckbox")
+        password = password_generator(charset_chosen=charset_chosen)
+        return escape("Your password is: " + password)
+    return render_template("checkboxes.html")
