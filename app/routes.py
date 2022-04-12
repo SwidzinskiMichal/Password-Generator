b646f2cc77d956edf0e0e9c4f391426b5c2bd40f
Michał Świdziński <michalswidzinski01@gmail.com>
2022-04-12 21:02:27 +0200

    Code Refactor

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,9 @@ from app.generate_password import generate_password
 
 
 @app.route('/', methods=['GET', 'POST'])
-def checkboxes():
+def index():
     if request.method == "POST":
-        charset_chosen = request.form.getlist("mycheckbox")
+        charset_chosen = request.form.getlist("charsets")
         password = generate_password(charset_chosen=charset_chosen)
         return escape("Your password is: " + password)
-    return render_template("checkboxes.html")
+    return render_template("index.html")
