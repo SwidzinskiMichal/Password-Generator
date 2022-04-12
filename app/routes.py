9166c7b2880346d1e80a240a0a32b2fc3baec9e5
Michał Świdziński <michalswidzinski01@gmail.com>
2022-04-12 21:20:05 +0200

    Add option to return to home page

--- app/routes.py
+++ app/routes.py
@@ -8,5 +8,6 @@ def index():
     if request.method == "POST":
         charset_chosen = request.form.getlist("charsets")
         password = generate_password(charset_chosen=charset_chosen)
-        return escape("Your password is: " + password)
+        return render_template("view_password.html", password=password)
+       # return escape("Your password is: " + password)
     return render_template("index.html")
